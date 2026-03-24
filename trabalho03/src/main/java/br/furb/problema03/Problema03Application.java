7fac68e39cc7340605fb0090e5b03dc4ff533269
LucasHenschel <lucas101henschel@gmail.com>
2026-03-23 21:08:58 -0300

    feat: Added initial struct

---
+++ trabalho03/src/main/java/br/furb/problema03/Problema03Application.java
@@ -0,0 +1,13 @@
+package br.furb.problema03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Problema03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Problema03Application.class, args);
+    }
+
+}
